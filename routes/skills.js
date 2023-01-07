51b48163e17ad196aa27263a9247a9d86aa931b4
CaptMerica <rmerica@live.com>
2023-01-07 12:55:24 -0500

    Update routing

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,18 @@
 import { Router } from 'express'
+import { skills } from '../data/skill-data.js'
+import { app } from '../server'
 
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource')
+})
+
+app.get('/', function(req, res) {
+  res.render('skills/index', {
+    skills: skills
+  })
 })
 
 export {
