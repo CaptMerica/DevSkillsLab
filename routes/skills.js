0b90b42d49d86c996e87342dc35cc5daa41febc5
CaptMerica <rmerica@live.com>
2023-01-08 21:40:58 -0500

    Update controllers/skills.js

--- routes/skills.js
+++ routes/skills.js
@@ -1,20 +1,7 @@
 import { Router } from 'express'
-import { skills } from '../data/skill-data.js'
-import { app } from '../server'
 import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
-
-app.get('/', function(req, res) {
-  res.render('skills/index', {
-    skills: skills
-  })
-})
 
 router.get('/', skillsCtrl.index)
 
