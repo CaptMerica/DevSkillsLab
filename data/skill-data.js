ef613f2599d004e276b0d48d30123af45d685967
CaptMerica <rmerica@live.com>
2023-01-08 23:05:40 -0500

    Update mongoose

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,7 +1,7 @@
 const skills = [
-  {text: 'Feed llama', done: true, _id: 125223},
-  {text: 'Sleep under the stars', done: false, _id: 127904},
-  {text: 'Buy milk', done: false, _id: 139608},
+  {text: 'Practice guitar for 2 hours', done: true, _id: 125223},
+  {text: 'Learn MEN database modules', done: false, _id: 127904},
+  {text: 'Write new Backslide riffs', done: false, _id: 139608},
 ]
 
 export { 
